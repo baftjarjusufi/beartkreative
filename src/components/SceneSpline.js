861803525ec25fc038ee8acc4a3a420c61f8ee41
baftjarjusufi <baftjarjj@gmail.com>
2025-05-01 13:57:24 +0200

    ''3d new stuff...''

--- src/components/SceneSpline.js
+++ src/components/SceneSpline.js
@@ -8,7 +8,7 @@ const { width, height } = Dimensions.get('window');
 const SceneSpline = () => {
     return (
         <View style={styles.splineWrapper}>
-            <Spline scene="/scene1.splinecode" />
+            <Spline scene="/scene3.splinecode" />
         </View>
     );
 };
